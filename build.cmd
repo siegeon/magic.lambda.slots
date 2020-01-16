7a9a4af482d304b342d9c789034cc856a28e0cd1
Thomas Hansen <thomas@gaiasoul.com>
2020-01-16 17:25:57 +0200

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,2 +1,8 @@
+
+set version=%1
+set key=%2
+
 cd %~dp0
+
 dotnet build magic.lambda.slots/magic.lambda.slots.csproj --configuration Release --source https://api.nuget.org/v3/index.json
+dotnet nuget push magic.lambda.slots/bin/Release/magic.lambda.slots.%version%.nupkg -k %key% -s https://api.nuget.org/v3/index.json
