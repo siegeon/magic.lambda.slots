4ad1a1a461a1f8567832954be552b86c26d46730
Thomas Hansen <thomas@gaiasoul.com>
2020-05-29 14:25:14 +0300

    Update ReturnNodes.cs

--- magic.lambda.slots/ReturnNodes.cs
+++ magic.lambda.slots/ReturnNodes.cs
@@ -10,6 +10,7 @@ using magic.signals.contracts;
 
 namespace magic.lambda.slots
 {
+    // TODO: Create simpler return keywords, such as e.g. simply [return].
     /// <summary>
     /// [slots.return-nodes] slot for returning nodes from some evaluation object.
     /// </summary>
